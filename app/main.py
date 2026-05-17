3c75a70034e1c9e8363da1966e5e2b4456b0ab5b
Chaitra Godase <godasechaitra@gmail.com>
2026-05-17 23:31:07 +0530

    smart route

--- app/main.py
+++ app/main.py
@@ -203,4 +203,5 @@ async def generate_smart_route(req: RouteRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
